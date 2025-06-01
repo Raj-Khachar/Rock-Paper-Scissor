8389f39544b0a377463f708f594d4534cb3235a7
Raj <rajkhachar700@gmail.com>
2025-06-01 19:06:11 +0530

    Update RockPaperScissors.js

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,5 +1,3 @@
-
-
 const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
@@ -32,7 +30,6 @@ function playGame(playerChoice) {
     playerDisplay.textContent = `PLAYER: ${playerChoice}`;
     computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
     resultDisplay.textContent = result;
-
     resultDisplay.classList.remove("greenText", "redText");
 
     switch(result){
@@ -48,72 +45,3 @@ function playGame(playerChoice) {
             break;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*				original but it has fault
-
-
-
-const choices = ["rock", "paper", "scissors"];
-const playerDisplay = document.getElementById("playerDisplay");
-const computerDisplay = document.getElementById("computerDisplay");
-const resultDisplay = document.getElementById("resultDisplay");
-const playerScoreDisplay = document.getElementById("playerScoreDisplay");
-const computerScoreDisplay = document.getElementById("computerScoreDisplay");
-let playerScore = 0;
-let computerScore = 0;
-
-function playGame(playerChoice) {
-	const computerChoice = choices[Math.floor(Math.random() * 3)];
-	let result = "";
-
-	if (playerChoice === computerChoice) {
-		result = "IT'S A TIE";
-	}
-	else{
-		switch(playerChoice){
-		case "rock":
-			result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
-			break;
-		case "paper":
-			result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
-			break;
-		case "scissors":
-			result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
-			break;
-			}
-		}
-	}
-	playerDisplay.textContent = `PLAYER: ${playerChoice}`;
-	computerDisplay.textContent = `Computer: ${computerChoice}`;
-	resultDisplay.textContent = result;
-
-	resultDisplay.classList.remove("greenText", "redText");
-
-	switch(result){
-	case "YOU WIN!":
-		resultDisplay.classList.add("greenText");
-		playerScore++;
-		playerScoreDisplay.textContent = playerScore;
-		break;
-	case "YOU LOSE!":
-		resultDisplay.classList.add("redText");
-		computerScore++;
-		computerScoreDisplay.textContent = computerScore;
-		break;
-	}
-}
-*/
-
